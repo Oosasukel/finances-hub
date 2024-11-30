0292dccf189a74d5bbb4bb0f850b4c292a7d11f9
Rodrigo Gonçalves <rodrigo.nascimento@dasa.com.br>
2024-11-30 08:18:49 -0300

    finances blog

---
+++ docusaurus.config.ts
@@ -0,0 +1,73 @@
+import type * as Preset from '@docusaurus/preset-classic';
+import type { Config } from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+
+const config: Config = {
+  title: 'Finances Blog',
+  tagline: 'Blog sobre finanças pessoais e investimentos',
+  favicon: 'img/favicon.png',
+
+  // Set the production url of your site here
+  url: 'https://finances.rodrigohub.com',
+
+  baseUrl: '/',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'pt-BR',
+    locales: ['pt-BR'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: false,
+        blog: {
+          routeBasePath: '/blog',
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    navbar: {
+      title: 'Finances Blog',
+      logo: {
+        alt: 'Finances Blog Logo',
+        src: 'img/favicon.png',
+      },
+      items: [
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/Oosasukel/finances-hub',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'light',
+      copyright: `Copyright © ${new Date().getFullYear()} Rodrigo Gonçalves`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
